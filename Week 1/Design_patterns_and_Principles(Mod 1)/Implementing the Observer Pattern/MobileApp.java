f5b31a61cf40d8f7044cb6d3e58b1f974ecb844d
varshinikuppam <120080069+Varshini2801@users.noreply.github.com>
2025-06-21 10:37:14 +0530

    Create MobileApp.java

---
+++ Week 1/Design_patterns_and_Principles(Mod 1)/Implementing the Observer Pattern/MobileApp.java
@@ -0,0 +1,12 @@
+public class MobileApp implements Observer {
+    private String appId;
+
+    public MobileApp(String appId) {
+        this.appId = appId;
+    }
+
+    @Override
+    public void update(String stockName, double stockPrice) {
+        System.out.println("MobileApp " + appId + ": Stock " + stockName + " updated to ₹" + stockPrice);
+    }
+}
