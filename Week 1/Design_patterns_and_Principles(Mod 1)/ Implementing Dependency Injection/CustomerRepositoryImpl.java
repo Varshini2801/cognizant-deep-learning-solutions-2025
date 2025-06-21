40ee8cbc62297d8ac2c04667cbf305c14d0f36cc
varshinikuppam <120080069+Varshini2801@users.noreply.github.com>
2025-06-21 10:49:47 +0530

    Create CustomerRepositoryImpl.java

---
+++ Week 1/Design_patterns_and_Principles(Mod 1)/ Implementing Dependency Injection/CustomerRepositoryImpl.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+    private Map<String, Customer> customerData = new HashMap<>();
+
+    public CustomerRepositoryImpl() {
+        // Simulating a data source
+        customerData.put("C101", new Customer("C101", "Alice"));
+        customerData.put("C102", new Customer("C102", "Bob"));
+    }
+
+    @Override
+    public Customer findCustomerById(String id) {
+        return customerData.get(id);
+    }
+}
