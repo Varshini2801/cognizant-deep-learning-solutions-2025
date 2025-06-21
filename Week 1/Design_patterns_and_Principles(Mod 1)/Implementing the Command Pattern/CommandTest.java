0f3f4482e5cc78a4f1d6d6c0d58a32e4a0ef5e75
varshinikuppam <120080069+Varshini2801@users.noreply.github.com>
2025-06-21 10:45:01 +0530

    Create CommandTest.java

---
+++ Week 1/Design_patterns_and_Principles(Mod 1)/Implementing the Command Pattern/CommandTest.java
@@ -0,0 +1,18 @@
+public class CommandTest {
+    public static void main(String[] args) {
+        Light livingRoomLight = new Light();
+
+        Command lightOn = new LightOnCommand(livingRoomLight);
+        Command lightOff = new LightOffCommand(livingRoomLight);
+
+        RemoteControl remote = new RemoteControl();
+
+        System.out.println("Pressing ON button:");
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        System.out.println("Pressing OFF button:");
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
