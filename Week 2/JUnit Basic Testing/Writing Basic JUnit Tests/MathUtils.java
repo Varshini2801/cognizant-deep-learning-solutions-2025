f1afdcb0107b33c5042a11696882634f65a6bd74
varshinikuppam <120080069+Varshini2801@users.noreply.github.com>
2025-06-26 19:26:29 +0530

    Create MathUtils.java

---
+++ Week 2/JUnit Basic Testing/Writing Basic JUnit Tests/MathUtils.java
@@ -0,0 +1,14 @@
+public class MathUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
