031a4889e82803e945290f7e43581dffc22a9962
varshinikuppam <120080069+Varshini2801@users.noreply.github.com>
2025-06-26 19:25:00 +0530

    Create CalculatorTest.java

---
+++ Week 2/JUnit Basic Testing/Setting Up JUnit/CalculatorTest.java
@@ -0,0 +1,13 @@
+File: src/test/java/CalculatorTest.java
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        int result = calc.add(2, 3);
+        assertEquals(5, result);
+    }
+}
