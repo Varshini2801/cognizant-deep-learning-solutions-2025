c93cec4734dd63f80ca8cf028f1753568e62c92d
varshinikuppam <120080069+Varshini2801@users.noreply.github.com>
2025-06-26 13:34:55 +0530

    Create customer_management_body.sql

---
+++ Week 2/PLSQL/Packages/customer_management_body.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE PACKAGE BODY CustomerManagement AS
+
+    PROCEDURE AddCustomer(p_id NUMBER, p_name VARCHAR2, p_dob DATE, p_balance NUMBER) IS
+    BEGIN
+        INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+        VALUES (p_id, p_name, p_dob, p_balance, SYSDATE);
+    END;
+
+    PROCEDURE UpdateCustomer(p_id NUMBER, p_name VARCHAR2, p_balance NUMBER) IS
+    BEGIN
+        UPDATE Customers
+        SET Name = p_name,
+            Balance = p_balance,
+            LastModified = SYSDATE
+        WHERE CustomerID = p_id;
+    END;
+
+    FUNCTION GetCustomerBalance(p_id NUMBER) RETURN NUMBER IS
+        v_balance NUMBER;
+    BEGIN
+        SELECT Balance INTO v_balance FROM Customers WHERE CustomerID = p_id;
+        RETURN v_balance;
+    END;
+
+END CustomerManagement;
