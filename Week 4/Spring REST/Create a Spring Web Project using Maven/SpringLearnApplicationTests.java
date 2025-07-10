002bb2f575f81b14689093d857a6dce0292d7a21
varshinikuppam <120080069+Varshini2801@users.noreply.github.com>
2025-07-10 22:03:17 +0530

    Create SpringLearnApplicationTests.java

---
+++ Week 4/Spring REST/Create a Spring Web Project using Maven/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpringLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Placeholder test
+        assertTrue(true);
+    }
+}
