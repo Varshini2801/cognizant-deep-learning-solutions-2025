d2975da03ad40ff17d695c48ef0224460ea0fe7f
varshinikuppam <120080069+Varshini2801@users.noreply.github.com>
2025-07-10 22:26:36 +0530

    Create JwtUtil.java

---
+++ Week 4/JWT/Create authentication service that returns JWT/JwtUtil.java
@@ -0,0 +1,24 @@
+// JwtUtil.java
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuer("AuthApp")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000)) // 1 hour
+                .signWith(key)
+                .compact();
+    }
+}
