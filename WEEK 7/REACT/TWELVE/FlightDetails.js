6436f87194ae1a433b1e1deca6c1e1bab7d5fb4b
varshinikuppam <120080069+Varshini2801@users.noreply.github.com>
2025-08-02 21:42:07 +0530

    Create FlightDetails.js

---
+++ WEEK 7/REACT/TWELVE/FlightDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FlightDetails = () => {
+  return (
+    <div>
+      <h3>Flight Details</h3>
+      <ul>
+        <li>Flight: AI202 – Bangalore to Delhi – 9:00 AM</li>
+        <li>Flight: SG303 – Chennai to Mumbai – 1:00 PM</li>
+        <li>Flight: UK404 – Hyderabad to Goa – 6:00 PM</li>
+      </ul>
+    </div>
+  );
+};
+
+export default FlightDetails;
